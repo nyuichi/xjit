060871abef2020e035ec4770d2a84dd64cb09285
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-03-18 15:41:23 +0900

    initial code commit

---
+++ include/xjit.h
@@ -0,0 +1,114 @@
+#ifndef XJIT_H__
+#define XJIT_H__
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#ifndef __x86_64__
+# error unsupported platform
+#endif
+
+#include <stdint.h>
+
+typedef struct xJIT xJIT;
+typedef struct xOperand xOperand;
+typedef struct xReg xReg;
+
+void xjit_init(xJIT *);
+void xjit_ready(xJIT *);
+const void *xjit_getcode(xJIT *);
+void xjit_destroy(xJIT *);
+
+#define xjit_ptr(...) xjit_addr_mode(__VA_ARGS__, xjit_bdxs, xjit_dbx, xjit_db, xjit_d)(__VA_ARGS__)
+
+#define xjit_rax (xjit_rax_())
+#define xjit_rbx (xjit_rbx_())
+#define xjit_rcx (xjit_rcx_())
+#define xjit_rdx (xjit_rdx_())
+#define xjit_rsp (xjit_rsp_())
+#define xjit_rdp (xjit_rdp_())
+#define xjit_rsi (xjit_rsi_())
+#define xjit_rdi (xjit_rdi_())
+
+enum {
+  XJIT_CALL_ABS,
+  XJIT_CALL_LABEL
+};
+
+enum {
+  XJIT_JMP_ABS,
+  XJIT_JMP_LABEL
+};
+
+void xjit_push(xJIT *, const xOperand *);
+void xjit_pop(xJIT *, const xOperand *);
+void xjit_mov(xJIT *, const xOperand *, const xOperand *);
+void xjit_call(xJIT *, const void *, uint8_t);
+void xjit_ret(xJIT *);
+void xjit_inc(xJIT *, const xOperand *);
+void xjit_dec(xJIT *, const xOperand *);
+void xjit_add(xJIT *, const xOperand *, const xOperand *);
+void xjit_addi(xJIT *, const xOperand *, uint32_t);
+void xjit_sub(xJIT *, const xOperand *, const xOperand *);
+void xjit_subi(xJIT *, const xOperand *, uint32_t);
+void xjit_test(xJIT *, const xOperand *, const xReg *);
+void xjit_testi(xJIT *, const xOperand *, uint32_t);
+void xjit_jmp(xJIT *, const void *, uint8_t);
+void xjit_jz(xJIT *, const void *);
+
+/* alias */
+
+#if XJIT_ALIAS
+
+# define ptr xjit_ptr
+
+# define rax xjit_rax
+# define rbx xjit_rbx
+# define rcx xjit_rcx
+# define rdx xjit_rdx
+# define rsp xjit_rsp
+# define rbp xjit_rbp
+# define rsi xjit_rsi
+# define rdi xjit_rdi
+
+# define push xjit_push
+# define pop xjit_pop
+# define mov xjit_mov
+# define call xjit_call
+# define ret xjit_ret
+# define inc xjit_inc
+# define dec xjit_dec
+# define add xjit_add
+# define addi xjit_addi
+# define sub xjit_sub
+# define subi xjit_subi
+# define test xjit_test
+# define testi xjit_testi
+# define jmp xjit_jmp
+# define jz xjit_jz
+
+#endif
+
+/* private interfaces */
+
+#define xjit_addr_mode(_1,_2,_3,_4,name,...) name
+xOperand *xjit_b(const xReg *base);
+xOperand *xjit_bd(const xReg *base, uint32_t disp);
+xOperand *xjit_bdx(const xReg *base, uint32_t disp, const xReg *index);
+xOperand *xjit_bdxs(const xReg *base, uint32_t disp, const xReg *index, uint8_t scale);
+
+xOperand *xjit_rax_(void);
+xOperand *xjit_rbx_(void);
+xOperand *xjit_rcx_(void);
+xOperand *xjit_rdx_(void);
+xOperand *xjit_rsp_(void);
+xOperand *xjit_rbp_(void);
+xOperand *xjit_rsi_(void);
+xOperand *xjit_rdi_(void);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
---
+++ src/xjit.cc
@@ -0,0 +1,97 @@
+#include "xjit.h"
+
+#define XBYAK_NO_OP_NAMES
+#include "xbyak/xbyak.h"
+
+extern "C" {
+
+struct xJIT {
+  Xbyak::CodeGenerator *codegen;
+};
+
+struct xOperand {
+  xOperand(const Xbyak::Operand &op) : op(op) {}
+  Xbyak::Operand op;
+};
+
+struct xReg {
+  Xbyak::Reg reg;
+};
+
+void
+xjit_init(xJIT *xjit)
+{
+  xjit->codegen = new Xbyak::CodeGenerator(100);
+}
+
+void
+xjit_ready(xJIT *xjit)
+{
+  xjit->codegen->ready();
+}
+
+const void *
+xjit_getcode(xJIT *xjit)
+{
+  return xjit->codegen->getCode();
+}
+
+void
+xjit_destroy(xJIT *xjit)
+{
+  delete xjit->codegen;
+}
+
+xOperand *xjit_b(const xReg *base) { return new xOperand(Xbyak::util::ptr[base->reg]); }
+xOperand *xjit_bd(const xReg *base, uint32_t disp) { return new xOperand(Xbyak::util::ptr[base->reg + disp]); }
+xOperand *xjit_bdx(const xReg *base, uint32_t disp, const xReg *index) { return new xOperand(Xbyak::util::ptr[base->reg + index->reg + disp]); }
+xOperand *xjit_bdxs(const xReg *base, uint32_t disp, const xReg *index, uint8_t scale) { return new xOperand(Xbyak::util::ptr[base->reg + index->reg * scale + disp]); }
+
+xOperand *xjit_rax_(void) { return new xOperand(Xbyak::util::rax); }
+xOperand *xjit_rbx_(void) { return new xOperand(Xbyak::util::rbx); }
+xOperand *xjit_rcx_(void) { return new xOperand(Xbyak::util::rcx); }
+xOperand *xjit_rdx_(void) { return new xOperand(Xbyak::util::rdx); }
+xOperand *xjit_rsp_(void) { return new xOperand(Xbyak::util::rsp); }
+xOperand *xjit_rbp_(void) { return new xOperand(Xbyak::util::rbp); }
+xOperand *xjit_rsi_(void) { return new xOperand(Xbyak::util::rsi); }
+xOperand *xjit_rdi_(void) { return new xOperand(Xbyak::util::rdi); }
+
+void xjit_push(xJIT *xjit, const xOperand *op) { xjit->codegen->push(op->op); }
+void xjit_pop(xJIT *xjit, const xOperand *op) { xjit->codegen->pop(op->op); }
+void xjit_mov(xJIT *xjit, const xOperand *op1, const xOperand *op2) { xjit->codegen->mov(op1->op, op2->op); }
+void xjit_call(xJIT *xjit, const void *op, uint8_t mode) {
+  switch (mode) {
+  case XJIT_CALL_LABEL:
+    xjit->codegen->call(static_cast<const char *>(op));
+    break;
+  case XJIT_CALL_ABS:
+    xjit->codegen->call(op);
+    break;
+  }
+}
+void xjit_ret(xJIT *xjit) { xjit->codegen->ret(); }
+void xjit_inc(xJIT *xjit, const xOperand *op) { xjit->codegen->inc(op->op); }
+void xjit_dec(xJIT *xjit, const xOperand *op) { xjit->codegen->dec(op->op); }
+void xjit_add(xJIT *xjit, const xOperand *op1, const xOperand *op2) { xjit->codegen->add(op1->op, op2->op); }
+void xjit_addi(xJIT *xjit, const xOperand *op, uint32_t imm) { xjit->codegen->add(op->op, imm); }
+void xjit_sub(xJIT *xjit, const xOperand *op1, const xOperand *op2) { xjit->codegen->sub(op1->op, op2->op); }
+void xjit_subi(xJIT *xjit, const xOperand *op, uint32_t imm) { xjit->codegen->sub(op->op, imm); }
+void xjit_test(xJIT *xjit, const xOperand *op, const xReg *reg) { xjit->codegen->test(op->op, reg->reg); }
+void xjit_testi(xJIT *xjit, const xOperand *op, uint32_t imm) { xjit->codegen->test(op->op, imm); }
+void xjit_jmp(xJIT *xjit, const void *op, uint8_t mode) {
+  switch (mode) {
+  case XJIT_JMP_LABEL:
+    xjit->codegen->jmp(static_cast<const char *>(op));
+    break;
+  case XJIT_JMP_ABS:
+    xjit->codegen->jmp(op);
+    break;
+  }
+}
+void xjit_jz(xJIT *xjit, const void *op) { xjit->codegen->jz(static_cast<const char *>(op)); }
+
+}
+
+// Local Variables:
+// flycheck-clang-include-path: ("../include" "../extlib/xbyak")
+// End:
